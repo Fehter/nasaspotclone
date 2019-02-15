9b7c8ce2c6986627bd3e306d6be81725a843b13a
mtlbaur <42392634+mtlbaur@users.noreply.github.com>
2019-02-15 16:45:35 -0500

    Added files for prototype GUI

---
+++ test_xamarin_app_v_1/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace test_xamarin_app
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage()
+		{
+			InitializeComponent();
+		}
+
+        // Then the Login button (which is defined in the LoginPage.xaml file) is clicked, push the HomePage onto the stack of pages.
+        private async void Login_Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HomePage());
+        }        
+    }
+}
---
+++ test_xamarin_app_v_1/RequestViewModel.cs
@@ -0,0 +1,57 @@
+﻿//using Xamarin.Forms;
+
+namespace test_xamarin_app.ViewModels
+{
+    // This class defines an object that can be used to store all the information for a single request.
+    // It is called "RequestViewModel" instead of "Request" because I was trying (perhaps unsuccessfully) to follow the View <-> ViewModel <-> Model design.
+    // See this link for more information: https://docs.microsoft.com/en-us/xamarin/xamarin-forms/enterprise-application-patterns/mvvm#the-mvvm-pattern
+    public class RequestViewModel
+	{
+        // As I understand it, these "get set" functions are used by the View of the MVVM pattern to be able to access the data members of this class.
+        // Without them, it would not be possible to use something like:
+        // <Label Text="{Binding OrganizationName}" FontSize="{StaticResource contentFontSize}" TextColor="{StaticResource contentTextColor}" HorizontalOptions="Start" VerticalOptions="Center"/>
+        // in a .xaml file because the "{Binding OrganizationName}" would not be able to "get" the information.
+        // Take this with some salt though -- I'm not comfortable with this yet.
+        public string Name { get; set; }
+        public string OrganizationName { get; set; }
+        public string Email { get; set; }
+        public string PrimaryPhoneNumber { get; set; }
+        public string AlternativePhoneNumber { get; set; }
+        public string ContactTimes { get; set; }
+        public string PresentationLocation { get; set; } // This represents the School Address column
+        public string PresentationRequested { get; set; }
+        public string PresentationRotations { get; set; }
+        public string HandsOnActivity { get; set; }
+        public string GradeLevels { get; set; }
+        public string NumberOfStudents { get; set; }
+        public string ProposedDateAndTime { get; set; }
+        public string Supplies { get; set; }
+        public string TravelFee { get; set; } // This could be a boolean if the spreadsheet always contains either "Yes" or "No" for this column
+        public string AmbassadorStatus { get; set; } // This relates to the "I understand that ambassadors are not ... and I take responsibility for them ..." column of the spreadsheet. This field should always say "Yes"
+        public string OtherConcerns { get; set; }
+        public string PresentationAlternativeMethod { get; set; } // This relates to the "Interested in alternative ways to receive a presentation?" column.
+
+        public string GetContents() // Utility function to quickly get contents of all data members.
+        {
+            return
+                Name + "\n" +
+                OrganizationName + "\n" +
+                Email + "\n" +
+                PrimaryPhoneNumber + "\n" +
+                AlternativePhoneNumber + "\n" +
+                ContactTimes + "\n" +
+                PresentationLocation + "\n" +
+                PresentationRequested + "\n" +
+                PresentationRotations + "\n" +
+                HandsOnActivity + "\n" +
+                GradeLevels + "\n" +
+                NumberOfStudents + "\n" +
+                ProposedDateAndTime + "\n" +
+                Supplies + "\n" +
+                TravelFee + "\n" +
+                AmbassadorStatus + "\n" +
+                OtherConcerns + "\n" +
+                PresentationAlternativeMethod + "\n";
+        }
+    }   
+}
