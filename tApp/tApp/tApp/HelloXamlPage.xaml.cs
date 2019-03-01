335783bc4b67b99ba760bce706a970167f8e302f
Sampson, William <wsampson1@students.fairmontstate.edu>
2019-03-01 12:29:09 -0500

    got a working homepage with a timer. Wanted to try pushing a branch to github so pushed what I have currently. (Some files may be duplicates of mbaurs, cause I didnt delete them)

---
+++ tApp/tApp/tApp/HelloXamlPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace test_xamarin_app
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HelloXamlPage : ContentPage
+	{
+		public HelloXamlPage ()
+		{
+			InitializeComponent ();
+            Device.StartTimer(TimeSpan.FromSeconds(5), () =>
+            {
+                GoToLoginPage();
+
+                return false; // True = Repeat again, False = Stop the timer
+            });
+
+
+        }
+
+        //Loads login page after you tap on the image button (will try to get this to be the whole screen)
+        private async void OnImageButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LoginPage());
+        }
+        //For the timer made a seperate function that opens login page after timer
+        //The timer one didnt need arguments, so seperate function
+        private async void GoToLoginPage()
+        {
+            await Navigation.PushAsync(new LoginPage());
+
+        }
+    }
+}
---
+++ test_xamarin_app_v_11/HomePage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace test_xamarin_app
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();         
+        }
+
+        // The following "_Button_Clicked" methods catch events from the buttons created in the "HomePage.xaml" file.
+        private async void RequestsPage_Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RequestsPage());
+        }
+
+        private async void ListViewPage1_Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListViewPage1());
+        }
+
+        private async void HelloXamlPage_Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HelloXamlPage());
+        }            
+
+        private async void GridDemoPage_Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GridDemoPage());
+        }
+
+        // This function causes the application to go back to the root page by popping all non-root pages off the stack of pages.
+        // The root page is defined by the following line:
+        // MainPage = new NavigationPage(new LoginPage());
+        // which is located in the App.xaml.cs file.
+        private async void Logout_Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
